76e1fa978983690a295fefd44ee38d4a8665fbee
Ruisfutrue <2273709996@qq.com>
2017-10-10 03:06:11 -0500

    change some typo

--- first_practice/A.cpp
+++ first_practice/A.cpp
@@ -15,11 +15,11 @@ ll ways[55][55];
 int main(int argc, char const *argv[])
 {
 	int n;
-	int a,b;
-	scanf("%d",&n);
+	int a, b;
+	scanf("%d", &n);
 	for (int i = 0; i < n; ++i)
 	{
-		scanf("%d%d",&a, &b);
+		scanf("%d%d", &a, &b);
 		memset(ways, 0, sizeof(ways));
 		ways[a][a] = 1;
 		ways[a][a + 1] = 1;
