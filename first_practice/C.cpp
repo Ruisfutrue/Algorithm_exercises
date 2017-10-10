81978ad82fe475a01c3d8376106ed76f113644c8
Ruisfutrue <2273709996@qq.com>
2017-10-10 04:06:36 -0500

    Update C.cpp

--- first_practice/C.cpp
+++ first_practice/C.cpp
@@ -4,8 +4,7 @@
 using namespace std;
 
 struct Matrix {
-	Matrix(int a1 = 0, int b1 = 0, int c1 = 0, int d1 = 0)
-	{
+	Matrix(int a1 = 0, int b1 = 0, int c1 = 0, int d1 = 0) {
 		a = a1;
 		b = b1;
 		c = c1;
@@ -17,9 +16,7 @@ struct Matrix {
 Matrix base(1, 1, 1, 0);
 
 //计算两个矩阵的乘积
-Matrix cal_matrix(Matrix m1, Matrix m2)
-{
-
+Matrix cal_matrix(Matrix m1, Matrix m2) {
 	Matrix temp;
 	temp.a = (m1.a * m2.a + m1.b * m2.c) % 10007;
 	temp.b = (m1.a * m2.b + m1.b * m2.d) % 10007;
@@ -29,20 +26,14 @@ Matrix cal_matrix(Matrix m1, Matrix m2)
 }
 
 //计算base矩阵的n次方
-Matrix cal_fab(int n, Matrix base)
-{
-	if (n == 1)
-	{
+Matrix cal_fab(int n, Matrix base) {
+	if (n == 1) {
 		return base;
-	}
-	else
-	{
+	} else {
 		Matrix hello(1, 0, 0, 1);
 //快速幂
-		while (n > 1)
-		{
-			if (1 == n % 2)
-			{
+		while (n > 1) {
+			if (1 == n % 2) {
 				hello = cal_matrix(base, hello);
 			}
 			base = cal_matrix(base, base);
@@ -52,21 +43,16 @@ Matrix cal_fab(int n, Matrix base)
 	}
 }
 
-int main()
-{
+int main() {
 	int n;
-	while(1 == scanf("%d", &n))
-	{
-		if (0 == n)
-		{
+	while(1 == scanf("%d", &n)) {
+		if (0 == n) {
 			printf("0\n");
 		}
-		else if (1 == n)
-		{
+		else if (1 == n) {
 			printf("1\n");
 		}
-		else
-		{
+		else {
 			printf("%d\n", cal_fab(n - 1, base).a);
 		}
 	}
